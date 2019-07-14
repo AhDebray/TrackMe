844a3b7acb3758002df7c92b1b231a160cccc00e
AhDebray <adebray@deakin.edu.au>
2019-07-14 18:41:59 +1000

    ENHANCE:

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-ECHO is on.
+devices.forEach(function(device) 
+{
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+   });
+
+$('#add-device').on('click', function() 
+{
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = 'device-list.html';
+});
