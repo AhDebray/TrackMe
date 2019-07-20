3467d880b9771cfe43b19ed8336c3c46ae2ad255
AhDebray <adebray@deakin.edu.au>
2019-07-20 16:43:46 +1000

    ENHANCE: files updated

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-devices.forEach(function(device) 
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+devices.push({ user: "A", name: "A's device"});
+localStorage.setItem('devices', JSON.stringify(devices));
+
+devices.forEach(function (device) 
 {
     $('#devices tbody').append(`
     <tr>
-    <td>${device.user}</td>
-    <td>${device.name}</td>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
     </tr>`
     );
    });
 
-$('#add-device').on('click', function() 
-{
-    const user = $('#user').val();
-    const name = $('#name').val();
-    devices.push({ user, name });
+   $('#add-device').on('click', function() {
+    const newuser = $('#user').val();
+    const newname = $('#name').val();
+    devices.push({ user: newuser, name: newname });
     localStorage.setItem('devices', JSON.stringify(devices));
     location.href = 'device-list.html';
-});
+   });
+   
+
+$('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+   });
+   
